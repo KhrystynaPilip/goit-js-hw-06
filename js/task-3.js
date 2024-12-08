1a9f7c3116d190739d6fb531a195a13895d5e27e
Khrystyna Pilip <tina.hristi@gmail.com>
2024-12-08 17:59:29 +0100

    Made tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,36 @@
-function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content); 
-    padding = Number.parseFloat(padding);
-    border = Number.parseFloat(border);
-    const totalWidth = content + 2 * padding + 2 * border;  
-    return totalWidth;
-};
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+class StringBuilder {
+    #value;
+  
+    constructor(initialValue) {
+      this.#value = initialValue;
+    }
+  
+    getValue() {
+      return this.#value;
+    }
+  
+    padEnd(str) {
+      this.#value += str;
+    }
+  
+    padStart(str) {
+      this.#value = str + this.#value;
+    }
+  
+    padBoth(str) {
+      this.padStart(str);
+      this.padEnd(str);
+    }
+  }
+  
+  // Перевірка коректності роботи класу
+  
+  const builder = new StringBuilder(".");
+  console.log(builder.getValue()); // "."
+  builder.padStart("^");
+  console.log(builder.getValue()); // "^."
+  builder.padEnd("^");
+  console.log(builder.getValue()); // "^.^"
+  builder.padBoth("=");
+  console.log(builder.getValue()); // "=^.^="
+  
